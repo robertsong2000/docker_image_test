f04e60f118e80dd75a127717833249652015ae83
Song Luo <ls130203@alibaba-inc.com>
2024-09-25 16:13:10 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt update && apt install -y shadowsocks
+
+cat > /etc/shadowsocks.json << EOF
+{
+	"server":"0.0.0.0",
+	"server_port":1080,
+	"password":"luosong1983",
+	"method":"aes-256-cfb"
+}
+EOF
+
+ssserver -c /etc/shadowsocks.json -d start
